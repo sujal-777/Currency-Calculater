9e3b78e91d8f72047c821ddea778bdc94a3b0235
Sujal <palsujal707@gmail.com>
2024-12-08 19:42:06 +0530

    custom hook initialize and input box

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./App.css"
+
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-3xl bg bg-orange-400'>Currency app</h1>
+    </div>
+  )
+}
+
+export default App
